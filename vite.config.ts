ce1a7cdbc75794a1d28363a0ef046cb14b6096ca
Tumelo Makgolo <162381140+mpurs@users.noreply.github.com>
2026-03-26 10:29:16 +0200

    Configure Vite server settings and base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,12 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    server: {
+    port: 3000,
+    host: `0.0.0.0`,
+    },
     plugins: [react(), tailwindcss()],
+    base: `/online-voting-system/`,
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
